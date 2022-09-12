101e4de8435a68ec94400f1d25344d7522a6226f
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-09-12 06:13:41 +0200

    updated

--- app.js
+++ app.js
@@ -8,37 +8,68 @@ let container =document.querySelector(".container");
 container.addEventListener("click", (e)=>{
 
 
+
+
     if(e.target.classList.contains("five")){
+        
  
         let bill = document.querySelector(".number-input").value;
-        document.querySelector(".tip").innerText = bill * e.target.innerText
-  
+
+        document.querySelector(".tip").innerText = (bill * e.target.innerText /100) /document.querySelector(".people").value;
+
+        document.querySelector(".total").innerText = (bill / document.querySelector(".people").value + Number(document.querySelector(".tip").innerText));
+        
+
     }
     else if(e.target.classList.contains("ten")){
         console.log("ten");
+        let bill = document.querySelector(".number-input").value;
+
+        document.querySelector(".tip").innerText = (bill * e.target.innerText /100) /(document.querySelector(".people").value)
+
+        document.querySelector(".total").innerText = (bill / document.querySelector(".people").value + Number(document.querySelector(".tip").innerText));
+
+
 
     }
     else if(e.target.classList.contains("fifteen")){
+        let bill = document.querySelector(".number-input").value;
+        document.querySelector(".tip").innerText = (bill * e.target.innerText /100) /(document.querySelector(".people").value)
         console.log("fifteen");
 
+        document.querySelector(".total").innerText = (bill / document.querySelector(".people").value + Number(document.querySelector(".tip").innerText));
     }
     else if(e.target.classList.contains("twenty-five")){
+        let bill = document.querySelector(".number-input").value;
+        document.querySelector(".tip").innerText = (bill * e.target.innerText /100) /(document.querySelector(".people").value)
         console.log("twenty-five");
+
+        document.querySelector(".total").innerText = Number(bill / document.querySelector(".people").value + Number(document.querySelector(".tip").innerText));
+
 
     }
     else if(e.target.classList.contains("fifty")){
+        let bill = document.querySelector(".number-input").value;
+        document.querySelector(".tip").innerText = (bill * e.target.innerText /100) /(document.querySelector(".people").value)
         console.log("fifty");
+        document.querySelector(".total").innerText = (bill / document.querySelector(".people").value + Number(document.querySelector(".tip").innerText));
 
     }
     else if(e.target.classList.contains("custom")){
+
+        document.querySelector(".custom").style.display ="none"
+        let bill = document.querySelector(".number-input").value;
+        document.querySelector(".tip").innerText = (bill * e.target.innerText /100) /(document.querySelector(".people").value)
         console.log("custom");
 
-    }
-    else{
-        console.log("other")
+        document.querySelector(".total").innerText = (bill / document.querySelector(".people").value + Number(document.querySelector(".tip").innerText));
 
     }
 
+    else if(e.target.classList.contains("res")){
+        console.log("reserlfhvfkn");
 
-
+    }
 });
+
+
